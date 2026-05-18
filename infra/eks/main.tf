b7cb018562a4dfcf321fb39ef8dfcc553c66a8da
Malathi Shetty <113874244+malathi-shetty@users.noreply.github.com>
2026-05-18 21:56:47 +0530

    Disable private access for EKS cluster endpoint

--- infra/eks/main.tf
+++ infra/eks/main.tf
@@ -19,7 +19,7 @@ module "eks" {
   ]
 
   cluster_endpoint_public_access  = true
-  cluster_endpoint_private_access = true
+  cluster_endpoint_private_access = false
 
   cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
 
