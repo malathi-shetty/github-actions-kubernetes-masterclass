1dee5e95c573f21b93598e292aec0e4efc259b30
Malathi Shetty <113874244+malathi-shetty@users.noreply.github.com>
2026-05-19 14:10:09 +0530

    Update EKS node group size configuration

--- infra/eks/main.tf
+++ infra/eks/main.tf
@@ -27,8 +27,8 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      desired_size   = 1
-      min_size       = 0
+      desired_size   = 2
+      min_size       = 1
       max_size       = 3
 
       instance_types = ["t3.xlarge"]
