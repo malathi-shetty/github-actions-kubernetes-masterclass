3be5edc735c7849348555e01d48b32e6ff7c9757
Malathi Shetty <113874244+malathi-shetty@users.noreply.github.com>
2026-05-19 13:46:15 +0530

    Add EKS node group configuration

--- infra/eks/main.tf
+++ infra/eks/main.tf
@@ -31,4 +31,19 @@ module "eks" {
       instance_types = ["t3.xlarge"]
     }
   }
+
+resource "aws_eks_node_group" "skillpulse" {
+  cluster_name    = aws_eks_cluster.skillpulse.name
+  node_group_name = "skillpulse-ng"
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = var.subnet_ids
+
+  instance_types = ["t3.xlarge"]
+
+  scaling_config {
+    desired_size = 2
+    min_size     = 1
+    max_size     = 3
+  }
+}
 }
